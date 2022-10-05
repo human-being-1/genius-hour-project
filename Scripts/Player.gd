141831da64ef6c4f0a1448db811101559e2611e5
monkey-GITHUB <32246286+monkey-GITHUB@users.noreply.github.com>
2022-10-05 14:17:26 -0400

    Change movement back to using the mouse/touchscreen

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -11,24 +11,24 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+#	var velocity = Vector2.ZERO
+#	if Input.is_action_pressed("move_up"):
+#		velocity.y -= 1
+#	if Input.is_action_pressed("move_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("move_down"):
+#		velocity.y += 1
+#	if Input.is_action_pressed("move_right"):
+#		velocity.x += 1
+#
+#	if velocity.length() > 0:
+#		velocity = velocity.normalized() * speed
 	#	$Sprite.play()
 	#else:
 	#	$Sprite.stop()
 
-	position += velocity * delta
-#	position = get_viewport().get_mouse_position()
+#	position += velocity * delta
+	position = get_viewport().get_mouse_position()
 	position.x = clamp(position.x, 1, screen_size.x)
 	position.y = clamp(position.y, 1, screen_size.y)
 	
