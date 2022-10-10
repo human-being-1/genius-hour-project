2155ea75627761ebe1edd4430188ee1812847202
monkey-GITHUB <32246286+monkey-GITHUB@users.noreply.github.com>
2022-10-10 16:02:12 -0400

    forgor to commit yesterday, code from Oct 9 2022

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -37,7 +37,7 @@ func _on_MobTimer_timeout():
 	mob.position = mob_spawn_location.position
 	
 	var direction = atan2(mob.position.direction_to($Player.position).y, mob.position.direction_to($Player.position).x)
-	direction += rand_range(-PI / 5, PI / 5)
+	direction += rand_range(-PI / 4.5, PI / 4.5)
 	
 	var velocity = Vector2(rand_range(min_speed, max_speed) + mob.speed_modifier, 0.0)
 	mob.linear_velocity = velocity.rotated(direction)
@@ -59,31 +59,31 @@ func _on_WaveTimer_timeout():
 	if $MobTimer.wait_time >= 0.55 and wave % 2 == 0:
 		$MobTimer.wait_time -= 0.10
 	
-	if $HUD/ProgressBar.max_value >= $Player.health + 1 and wave % 3 == 0:
+	if $HUD/ProgressBar.max_value >= $Player.health + 1 and wave % 2 == 0:
 		$Player.health += 1
 		$HUD/ProgressBar.value = $Player.health
 	
 	if wave == 4:
 		mobs.append(load("res://Scenes/GreenMob.tscn"))
-		$Player.health += 1
-		$HUD/ProgressBar.max_value = 6
+		$Player.health += 2
+		$HUD/ProgressBar.max_value = 7
 		$HUD/ProgressBar.value = $Player.health
 	
 	if wave == 8:
 		mobs.append(load("res://Scenes/YellowMob.tscn"))
 		$Player.health += 2
-		$HUD/ProgressBar.max_value = 8
+		$HUD/ProgressBar.max_value = 9
 		$HUD/ProgressBar.value = $Player.health
 	
 	if wave == 12:
 		mobs.append(load("res://Scenes/PurpleMob.tscn"))
 		$Player.health += 2
-		$HUD/ProgressBar.max_value = 10
+		$HUD/ProgressBar.max_value = 11
 		$HUD/ProgressBar.value = $Player.health
 	
 	if wave == 16:
 		mobs.append(load("res://Scenes/TealMob.tscn"))
-		$Player.health += 2
+		$Player.health += 1
 		$HUD/ProgressBar.max_value = 12
 		$HUD/ProgressBar.value = $Player.health
 	
